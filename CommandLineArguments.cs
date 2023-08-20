881c0209a82268015aa535207eeae393aa7f8b88
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2023-08-20 17:10:07 -0400

    Separated CLI into a partial class

---
+++ CommandLineArguments.cs
@@ -0,0 +1,29 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FreeRealmsUnpacker
+{
+    partial class Unpacker
+    {
+        [Argument(0, Description = "The Free Realms client directory."), Required, NotNull]
+        public string? InputDirectory { get; }
+
+        [Argument(1, Description = "The destination for extracted assets."), Required, NotNull]
+        public string? OutputDirectory { get; }
+
+        [Option(ShortName = "g", Description = "Extract game assets only (in 'Free Realms/')")]
+        public bool ExtractGame { get; }
+
+        [Option(ShortName = "t", Description = "Extract TCG assets only (in 'Free Realms/assets/')")]
+        public bool ExtractTCG { get; }
+
+        [Option(ShortName = "r", Description = "Extract resource assets only (in 'Free Realms/tcg/')")]
+        public bool ExtractResource { get; }
+
+        [Option(ShortName = "y", Description = "Automatically answer yes to any question.")]
+        public bool AnswerYes { get; }
+
+        public static int Main(string[] args) => CommandLineApplication.Execute<Unpacker>(args);
+    }
+}
--- Unpacker.cs
+++ Unpacker.cs
@@ -1,31 +1,13 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
-using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.CodeAnalysis;
 
 namespace FreeRealmsUnpacker
 {
-    public class Unpacker
+    public partial class Unpacker
     {
-        [Argument(0, Description = "The Free Realms client directory."), Required, NotNull]
-        public string? InputDirectory { get; }
-
-        [Argument(1, Description = "The destination for extracted assets."), Required, NotNull]
-        public string? OutputDirectory { get; }
-
-        [Option(ShortName = "g", Description = "Extract game assets only (in 'Free Realms/')")]
-        public bool ExtractGame { get; }
-
-        [Option(ShortName = "t", Description = "Extract TCG assets only (in 'Free Realms/assets/')")]
-        public bool ExtractTCG { get; }
-
-        [Option(ShortName = "r", Description = "Extract resource assets only (in 'Free Realms/tcg/')")]
-        public bool ExtractResource { get; }
-
-        [Option(ShortName = "y", Description = "Automatically answer yes to any question.")]
-        public bool AnswerYes { get; }
-
-        public static int Main(string[] args) => CommandLineApplication.Execute<Unpacker>(args);
-
+        /// <summary>
+        /// The entry point of <see href="FreeRealmsUnpacker"/>, following command parsing.
+        /// </summary>
+        /// <returns>The exit code.</returns>
         public int OnExecute()
         {
             // Set up the input and output directories.
