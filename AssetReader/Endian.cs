4116c927fd2928f2c6182809a65951840f4a0d50
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2023-09-23 09:50:25 -0400

    Created EndianBinaryReader class

---
+++ AssetReader/Endian.cs
@@ -0,0 +1,17 @@
+﻿namespace AssetReader
+{
+    /// <summary>
+    /// Indicates the order in which bytes are arranged into larger numerical values in memory.
+    /// </summary>
+    internal enum Endian
+    {
+        /// <summary>
+        /// The least significant byte is stored in the smallest address.
+        /// </summary>
+        Little,
+        /// <summary>
+        /// The most significant byte is stored in the smallest address.
+        /// </summary>
+        Big
+    }
+}
