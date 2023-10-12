439e1facbaf94a25ac38a6f8281a6385f2073fdc
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2023-10-12 16:32:53 -0400

    Created UnpackerGui project

---
+++ UnpackerGui/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Free Realms Unpacker")]
+[assembly: AssemblyProduct("FreeRealmsUnpackerGUI")]
---
+++ UnpackerGui/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace UnpackerGui.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
