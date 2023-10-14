c7776891b25cc0476c47662412e6861aece646c0
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2023-10-14 02:54:02 -0400

    Added checkbox list to select asset packs

--- UnpackerGui/ViewModels/MainViewModel.cs
+++ UnpackerGui/ViewModels/MainViewModel.cs
@@ -6,6 +6,8 @@ using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using UnpackerGui.Services;
@@ -17,12 +19,19 @@ public class MainViewModel : ViewModelBase
 {
     public MainViewModel()
     {
-        Assets = new ObservableCollection<Asset>();
+        Assets = new List<Asset>();
+        PackFiles = new ObservableCollection<PackFileViewModel>();
         AddPackFilesCommand = ReactiveCommand.CreateFromTask(AddPackFiles);
+        SelectAllCommand = ReactiveCommand.Create(SelectAll);
+        DeselectAllCommand = ReactiveCommand.Create(DeselectAll);
+        //Assets.CollectionChanged += (s, e) => Debug.WriteLine(Assets.Count);
     }
 
-    public ObservableCollection<Asset> Assets { get; }
+    public List<Asset> Assets { get; }
+    public ObservableCollection<PackFileViewModel> PackFiles { get; }
     public ICommand AddPackFilesCommand { get; }
+    public ICommand SelectAllCommand { get; }
+    public ICommand DeselectAllCommand { get; }
 
     private async Task AddPackFiles()
     {
@@ -36,7 +45,53 @@ public class MainViewModel : ViewModelBase
 
         foreach (IStorageFile file in files)
         {
-            Assets.AddRange(ClientFile.GetPackAssets(file.Path.LocalPath));
+            string path = file.Path.LocalPath;
+
+            if (PackFiles.All(x => x.Path != path))
+            {
+                PackFileViewModel packFile = new(path);
+                packFile.PropertyChanged += UpdateAssets;
+                PackFiles.Add(packFile);
+            }
+        }
+    }
+
+    private void UpdateAssets(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName is not nameof(PackFileViewModel.IsChecked)) return;
+
+        PackFileViewModel packFile = sender as PackFileViewModel
+            ?? throw new ArgumentException($"{nameof(sender)} must be a {nameof(PackFileViewModel)}.");
+
+        if (packFile.IsChecked)
+        {
+            Assets.AddRange(packFile.Assets);
+        }
+        else if (PackFiles.Count(x => x.IsChecked) > 1)
+        {
+            Assets.RemoveMany(packFile.Assets);
+        }
+        else
+        {
+            Assets.Clear();
+        }
+    }
+
+    private void SelectAll()
+    {
+        foreach (PackFileViewModel packFile in PackFiles)
+        {
+            packFile.IsChecked = true;
+        }
+    }
+
+    private void DeselectAll()
+    {
+        Assets.Clear();
+
+        foreach (PackFileViewModel packFile in PackFiles)
+        {
+            packFile.IsChecked = false;
         }
     }
 }
