c1c13870faec12753c8b6223919838e3621548c3
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2023-11-08 02:48:53 -0500

    Open asset when double-clicked in grid

--- UnpackerGui/Views/MainView.axaml.cs
+++ UnpackerGui/Views/MainView.axaml.cs
@@ -1,5 +1,11 @@
-﻿using Avalonia.Controls;
+﻿using AssetIO;
+using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
+using System;
+using System.Diagnostics;
+using System.IO;
+using UnpackerGui.Models;
 using UnpackerGui.ViewModels;
 
 namespace UnpackerGui.Views;
@@ -17,6 +23,24 @@ public partial class MainView : UserControl
         {
             mainViewModel.SelectedAssets.Items = assetGrid.SelectedItems;
             assetGrid.SelectionChanged += mainViewModel.SelectedAssets.OnCollectionChanged;
+        }
+    }
+
+    private void AssetGrid_DoubleTapped(object? sender, TappedEventArgs e)
+    {
+        if (assetGrid.SelectedItem is AssetInfo asset)
+        {
+            string tempDir = Path.GetTempPath();
+            FileInfo file = new(Path.Combine(tempDir, Guid.NewGuid().ToString(), asset.Name));
+            using AssetReader reader = asset.AssetFile.OpenRead();
+            file.Directory?.Create();
+            using FileStream fs = file.Open(FileMode.Create, FileAccess.Write, FileShare.Read);
+            reader.CopyTo(asset, fs);
+            Process.Start(new ProcessStartInfo
+            {
+                UseShellExecute = true,
+                FileName = file.FullName
+            });
         }
     }
 }
