9eafa879078f0abb18822b11fcf02ef0fbdfd23e
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2023-11-11 23:40:24 -0500

    Added search hotkeys

--- UnpackerGui/Views/MainView.axaml.cs
+++ UnpackerGui/Views/MainView.axaml.cs
@@ -21,15 +21,21 @@ public partial class MainView : UserControl
     {
         if (DataContext is MainViewModel mainViewModel)
         {
+            // SelectedItems is a not a bindable property in DataGrid, so we need to pass 
+            // it as a reference to the view model to keep track of the selected assets.
             mainViewModel.SelectedAssets.Items = assetGrid.SelectedItems;
             assetGrid.SelectionChanged += mainViewModel.SelectedAssets.OnCollectionChanged;
         }
+
+        // Globally handle key events from the window in this view.
+        KeyDownEvent.AddClassHandler<TopLevel>(MainView_KeyDown, handledEventsToo: true);
     }
 
     private void AssetGrid_DoubleTapped(object? sender, TappedEventArgs e)
     {
         if (assetGrid.SelectedItem is AssetInfo asset)
         {
+            // Extract the double-clicked asset to a temporary location and open it.
             string tempDir = Path.GetTempPath();
             FileInfo file = new(Path.Combine(tempDir, Guid.NewGuid().ToString(), asset.Name));
             using AssetReader reader = asset.AssetFile.OpenRead();
@@ -41,6 +47,24 @@ public partial class MainView : UserControl
                 UseShellExecute = true,
                 FileName = file.FullName
             });
+        }
+    }
+
+    private void MainView_KeyDown(object? sender, KeyEventArgs e)
+    {
+        if (this == sender) return;
+
+        switch (e.KeyModifiers, e.Key)
+        {
+            case (KeyModifiers.Alt, Key.C):
+                matchCaseButton.IsChecked ^= true;
+                break;
+            case (KeyModifiers.Alt, Key.E):
+                useRegexButton.IsChecked ^= true;
+                break;
+            case (KeyModifiers.Control, Key.F):
+                searchBarTextBox.Focus();
+                break;
         }
     }
 }
