e301186e7f01261470969107721c676bf56d45f0
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2025-06-17 00:26:09 -0400

    Moved secondary extract button to context menu

---
+++ UnpackerGui/Converters/ExpressionConverter.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace UnpackerGui.Converters;
+
+public class ExpressionConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) => value switch
+    {
+        int lhs when parameter is string rhs => rhs[0] switch
+        {
+            '>' => lhs > int.Parse(rhs.AsSpan(1)),
+            '<' => lhs < int.Parse(rhs.AsSpan(1)),
+            '=' => lhs == int.Parse(rhs.AsSpan(1)),
+            _ => new BindingNotification(new InvalidCastException(), BindingErrorType.Error)
+        },
+        _ => new BindingNotification(new InvalidCastException(), BindingErrorType.Error)
+    };
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => null;
+}
