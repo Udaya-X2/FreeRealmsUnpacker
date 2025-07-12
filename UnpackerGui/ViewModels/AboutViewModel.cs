6a6ccaacf4992773c2b8f8bbef89ef9cbc2c97f7
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2025-07-12 16:59:46 -0400

    Replaced link command with HyperlinkButton

--- UnpackerGui/ViewModels/AboutViewModel.cs
+++ UnpackerGui/ViewModels/AboutViewModel.cs
@@ -1,9 +1,4 @@
-﻿using ReactiveUI;
-using System;
-using System.Diagnostics;
-using System.Reactive;
-using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
 
 namespace UnpackerGui.ViewModels;
 
@@ -13,8 +8,6 @@ public class AboutViewModel : ViewModelBase
     public virtual string Copyright { get; }
     public virtual string SourceCodeUrl { get; }
 
-    public ReactiveCommand<string, Unit> OpenLinkCommand { get; }
-
     public AboutViewModel()
     {
         Assembly assembly = Assembly.GetEntryAssembly()!;
@@ -22,33 +15,5 @@ public class AboutViewModel : ViewModelBase
         Version = $"Version {version}";
         Copyright = assembly.GetCustomAttribute<AssemblyCopyrightAttribute>()!.Copyright;
         SourceCodeUrl = "https://github.com/Udaya-X2/FreeRealmsUnpacker";
-        OpenLinkCommand = ReactiveCommand.Create<string>(OpenLink);
-    }
-
-    /// <summary>
-    /// Opens the default browser to the specified URI.
-    /// </summary>
-    /// <remarks>
-    /// Source: <see href="https://brockallen.com/2016/09/24/process-start-for-urls-on-net-core/"/>
-    /// </remarks>
-    private static void OpenLink(string uri)
-    {
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            uri = uri.Replace("&", "^&");
-            Process.Start(new ProcessStartInfo(uri) { UseShellExecute = true });
-        }
-        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-        {
-            Process.Start("xdg-open", uri);
-        }
-        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-        {
-            Process.Start("open", uri);
-        }
-        else
-        {
-            throw new PlatformNotSupportedException();
-        }
     }
 }
