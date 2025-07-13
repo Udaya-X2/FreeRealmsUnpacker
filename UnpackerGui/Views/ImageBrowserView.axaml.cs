0912421be9a2f3946c2b9411d18b7edfbb159d32
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2025-07-13 10:54:03 -0400

    Added WIP image browser view

---
+++ UnpackerGui/Views/ImageBrowserView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace UnpackerGui.Views;
+
+public partial class ImageBrowserView : UserControl
+{
+    public ImageBrowserView()
+    {
+        InitializeComponent();
+    }
+}
