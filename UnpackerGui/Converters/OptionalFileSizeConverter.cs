8219f17b5cd87fe03b1e61bcdacd4b8124f739dd
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2025-07-15 19:47:49 -0400

    Use one column for both asset size representations

---
+++ UnpackerGui/Converters/OptionalFileSizeConverter.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace UnpackerGui.Converters;
+
+public class OptionalFileSizeConverter : IMultiValueConverter
+{
+    public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+        => values[1] switch
+        {
+            true => values[0]!.ToString(),
+            null => FileSizeConverter.GetFileSize((uint)values[0]!),
+            _ => null
+        };
+}
