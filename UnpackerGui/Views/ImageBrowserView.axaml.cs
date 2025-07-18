88b3dc8c7ba2b6a96703849e9ed2cbe736412fd8
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2025-07-18 01:13:31 -0400

    Added image zoom slider functionality

--- UnpackerGui/Views/ImageBrowserView.axaml.cs
+++ UnpackerGui/Views/ImageBrowserView.axaml.cs
@@ -1,4 +1,6 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
 
 namespace UnpackerGui.Views;
@@ -15,4 +17,13 @@ public partial class ImageBrowserView : UserControl
     private void Button_Click_ZoomOut(object? sender, RoutedEventArgs e) => zoomBorder.ZoomOut();
 
     private void Button_Click_ZoomIn(object? sender, RoutedEventArgs e) => zoomBorder.ZoomIn();
+
+    private void Slider_ValueChanged(object? sender, RangeBaseValueChangedEventArgs e)
+    {
+        if (e.NewValue != zoomBorder.ZoomX)
+        {
+            Rect bounds = zoomBorder.Child!.Bounds;
+            zoomBorder.Zoom(e.NewValue, bounds.Width / 2, bounds.Height / 2);
+        }
+    }
 }
