644f4ccb081bc42799d613c2af841807a822dfd4
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2025-07-18 22:22:56 -0400

    Fixed .jpg and other image types not showing

--- UnpackerGui/ViewModels/ImageOptionsViewModel.cs
+++ UnpackerGui/ViewModels/ImageOptionsViewModel.cs
@@ -1,8 +1,7 @@
-﻿using SkiaSharp;
-using System;
+﻿using System;
+using System.Collections.Frozen;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 
 namespace UnpackerGui.ViewModels;
 
@@ -12,10 +11,110 @@ namespace UnpackerGui.ViewModels;
 /// <typeparam name="T">The type of the item to filter.</typeparam>
 public class ImageOptionsViewModel<T> : FilterViewModel<T>
 {
-    private static readonly HashSet<string> s_imageFormats = [.. Enum.GetValues<SKEncodedImageFormat>()
-                                                                     .Except([SKEncodedImageFormat.Avif])
-                                                                     .Select(x => x.ToString().ToUpperInvariant())
-                                                                     .Concat(["DDS", "TGA"])];
+    private static readonly FrozenSet<string> s_imageFormats = ((HashSet<string>)
+    [
+        "3FR",
+        "AFPHOTO",
+        "AI",
+        "APNG",
+        "ARI",
+        "ARW",
+        "ASTC",
+        "AVIF",
+        "BAY",
+        "BMP",
+        "BPG",
+        "BRAW",
+        "CAP",
+        "CD5",
+        "CDR",
+        "CLIP",
+        "CPT",
+        "CR2",
+        "CR3",
+        "CRI",
+        "CRW",
+        "DCR",
+        "DCS",
+        "DDS",
+        "DIB",
+        "DNG",
+        "DRF",
+        "DXF",
+        "EIP",
+        "EMF",
+        "EPS",
+        "ERF",
+        "FFF",
+        "FITS",
+        "FLIF",
+        "GIF",
+        "GPR",
+        "HDR",
+        "HEIC",
+        "HEIF",
+        "ICO",
+        "IIQ",
+        "IND",
+        "INDD",
+        "INDT",
+        "J2K",
+        "JFI",
+        "JFIF",
+        "JIF",
+        "JP2",
+        "JPE",
+        "JPEG",
+        "JPF",
+        "JPG",
+        "JPM",
+        "JPX",
+        "JXL",
+        "K25",
+        "KDC",
+        "KRA",
+        "KTX",
+        "MDC",
+        "MDP",
+        "MEF",
+        "MJ2",
+        "MOS",
+        "MRW",
+        "NEF",
+        "NRW",
+        "ORF",
+        "PDF",
+        "PDN",
+        "PEF",
+        "PKM",
+        "PLD",
+        "PNG",
+        "PSD",
+        "PSP",
+        "PTX",
+        "PXN",
+        "R3D",
+        "RAF",
+        "RAW",
+        "RW2",
+        "RWL",
+        "RWZ",
+        "SAI",
+        "SR2",
+        "SRF",
+        "SRW",
+        "SVG",
+        "SVGZ",
+        "TCO",
+        "TGA",
+        "TIF",
+        "TIFF",
+        "WBMP",
+        "WEBP",
+        "WMF",
+        "X3F",
+        "XCF"
+    ]).ToFrozenSet();
 
     private readonly Func<T, string> _converter;
 
