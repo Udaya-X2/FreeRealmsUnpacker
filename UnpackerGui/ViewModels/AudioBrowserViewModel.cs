cf821e63cc52a0c2b15d64bd71f0314ae58a2cef
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2025-07-24 00:48:00 -0400

    Created WIP audio player controls

--- UnpackerGui/ViewModels/AudioBrowserViewModel.cs
+++ UnpackerGui/ViewModels/AudioBrowserViewModel.cs
@@ -1,4 +1,10 @@
-﻿using System.IO;
+﻿using AssetIO;
+using LibVLCSharp.Shared;
+using ReactiveUI;
+using System;
+using System.IO;
+using System.Reactive;
+using System.Threading.Tasks;
 using UnpackerGui.Collections;
 using UnpackerGui.Extensions;
 using UnpackerGui.Models;
@@ -10,39 +16,112 @@ public class AudioBrowserViewModel : AssetBrowserViewModel
     /// <inheritdoc/>
     public override FilteredReactiveCollection<AssetInfo> Assets { get; }
 
-    /// <summary>
-    /// Gets the media player.
-    /// </summary>
-    //public MediaPlayer MediaPlayer { get; }
+    public ReactiveCommand<Unit, Unit> TogglePlayPauseCommand { get; }
+    public ReactiveCommand<Unit, Unit> ToggleMuteCommand { get; }
+    public ReactiveCommand<string, Unit> SetRateCommand { get; }
 
-    //private readonly LibVLC _libVlc;
     private readonly MemoryStream _audioStream;
+    private readonly LibVLC _libVlc;
+    private readonly MediaPlayer _mediaPlayer;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="AudioBrowserViewModel"/> class.
     /// </summary>
     public AudioBrowserViewModel(ReadOnlyReactiveCollection<AssetInfo> assets)
     {
+        // Initialize each command.
+        TogglePlayPauseCommand = ReactiveCommand.Create(TogglePlayPause);
+        ToggleMuteCommand = ReactiveCommand.Create(ToggleMute);
+        SetRateCommand = ReactiveCommand.Create<string>(SetRate);
+
         // Filter the image assets from the asset browser.
         Assets = assets.Filter(x => x.IsAudio);
         OnAssetsInitialized(x => x.ShowAudioBrowser);
 
         // Initialize media player resources.
-        //_libVlc = new LibVLC();
         _audioStream = new MemoryStream();
-        //MediaPlayer = new MediaPlayer(_libVlc);
+        _libVlc = new LibVLC();
+        _mediaPlayer = new MediaPlayer(_libVlc);
+        _mediaPlayer.Paused += (s, e) => this.RaisePropertyChanged(nameof(IsPlaying));
+        _mediaPlayer.Stopped += (s, e) => this.RaisePropertyChanged(nameof(IsPlaying));
+        _mediaPlayer.Playing += (s, e) => this.RaisePropertyChanged(nameof(IsPlaying));
+        _mediaPlayer.LengthChanged += (s, e) => this.RaisePropertyChanged(nameof(Length));
+        _mediaPlayer.TimeChanged += (s, e) => this.RaisePropertyChanged(nameof(Time));
+        _mediaPlayer.VolumeChanged += (s, e) =>
+        {
+            this.RaisePropertyChanged(nameof(Volume));
+            this.RaisePropertyChanged(nameof(IsMuted));
+        };
+        _mediaPlayer.EndReached += (s, e) => Task.Run(() =>
+        {
+            _mediaPlayer.Stop();
+            this.RaisePropertyChanged(nameof(Time));
+        });
+
+        // Update the media to the selected asset.
+        this.WhenAnyValue(x => x.SelectedAsset)
+            .Subscribe(CreateMedia);
     }
 
-    //public void Play()
-    //{
-    //    AssetInfo asset = SelectedAsset!;
-    //    using AssetReader reader = asset.AssetFile.OpenRead();
-    //    _audioStream.SetLength(0);
-    //    reader.CopyTo(asset, _audioStream);
-    //    _audioStream.Position = 0;
-    //    using Media media = new(_libVlc, new StreamMediaInput(_audioStream));
-    //    MediaPlayer.Play(media);
-    //}
+    public bool IsPlaying => _mediaPlayer.IsPlaying;
 
-    //public void Stop() => MediaPlayer.Stop();
+    public bool IsMuted
+    {
+        get => _mediaPlayer.Volume == 0;
+        set => _mediaPlayer.Volume = value ? 0 : 100;
+    }
+
+    public int Volume
+    {
+        get => _mediaPlayer.Volume;
+        set => _mediaPlayer.Volume = value;
+    }
+
+    public long Time
+    {
+        get => _mediaPlayer.Time;
+        set => _mediaPlayer.Time = value;
+    }
+
+    public long Length => Math.Max(1, _mediaPlayer.Length);
+
+    public float Rate
+    {
+        get => _mediaPlayer.Rate;
+        set => _mediaPlayer.SetRate(value);
+    }
+
+    private void CreateMedia(AssetInfo? asset)
+    {
+        _mediaPlayer.Media = null;
+        this.RaisePropertyChanged(nameof(IsPlaying));
+        this.RaisePropertyChanged(nameof(Length));
+        this.RaisePropertyChanged(nameof(Time));
+
+        if (asset == null) return;
+
+        using AssetReader reader = asset.AssetFile.OpenRead();
+        _audioStream.SetLength(0);
+        reader.CopyTo(asset, _audioStream);
+        _audioStream.Position = 0;
+        using Media media = new(_libVlc, new StreamMediaInput(_audioStream));
+        _mediaPlayer.Media = media;
+        //_mediaPlayer.Play();
+    }
+
+    private void TogglePlayPause()
+    {
+        if (IsPlaying)
+        {
+            _mediaPlayer.Pause();
+        }
+        else
+        {
+            _mediaPlayer.Play();
+        }
+    }
+
+    private void ToggleMute() => IsMuted ^= true;
+
+    private void SetRate(string value) => _mediaPlayer.SetRate(float.Parse(value));
 }
