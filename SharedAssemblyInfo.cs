cc049e35ceaa070340ca387e51b1c8b589c30946
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2026-01-10 00:48:13 -0500

    Updated version number and assembly info

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Resources;
 
 [assembly: AssemblyCompany("Udaya")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyCopyright("Copyright © 2023-2025 Udaya")]
-[assembly: AssemblyFileVersion("2.1.0")]
-[assembly: AssemblyInformationalVersion("2.1.0")]
-[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyCopyright("Copyright © 2023-2026 Udaya")]
+[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0")]
+[assembly: AssemblyVersion("3.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
